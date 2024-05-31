c59e0d69e530941400ba1d67845397bc9f4d51fe
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-05-31 10:08:36 +0500

    sse

---
+++ skr.js
@@ -0,0 +1,13 @@
+let a=Math.round(Math.random()*99)
+let b=2*a
+let d=2/b
+let c=3/d
+console.log(b);
+alert(`siz hozir random sonlar chiqish boshlandi`)
+alert(`radom orali chiqan son ${a}`)
+confirm(`bir radom sonni 2ga kopaytimoqchi bolmoqchimiz!` )
+alert(`random orqalichiqan son: ${a} edi
+\n biz uni 2ga kopaytirib ${b}soni ciqardik
+\n va su soni 2ga bolib: ${b}sonini ciqardik
+\n va shu soni 3ga bolgandaki:${d}soni chiqardik.
+`)
